70715a7f200165ed85e1b50c55367a767c32e148
AlexBulatov <al.ross@yandex.ru>
2019-07-04 14:15:30 +0700

    Init commit

---
+++ src/main/java/ru/cft/shiftbanquet/ShiftbanquetApplication.java
@@ -0,0 +1,13 @@
+package ru.cft.shiftbanquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftbanquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftbanquetApplication.class, args);
+	}
+
+}
